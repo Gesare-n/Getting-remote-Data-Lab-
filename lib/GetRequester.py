1b8e0178b1e3f080c48a9c9a09a65a952957f457
nicoleonyiego <nicolegesare00@gmail.com>
2024-07-14 08:10:00 +0300

    assignment

---
+++ lib/GetRequester.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+class GetRequester:
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_response_body(self):
+        URL = self.url
+        response = requests.get(URL)
+        return response.content
+
+    def load_json(self):
+        URL = self.url
+        response = requests.get(URL)
+        return response.json()
